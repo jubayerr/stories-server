c85ac35931d2356815c5bac198a6749a79e0235e
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-06-20 10:22:44 +0600

    initial setup

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+import postRoutes from './routes/posts.js'
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+//     console.log("Connected to MongoDB");
+// });
+
+mongoose.set('useFindAndModify', false);
