467fc2d1a3a0f635537b4bb659994339f05aba51
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-06-21 00:39:19 +0600

    create post update post done

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js'
 
 export const getPosts = async (req, res) => {
@@ -6,7 +7,7 @@ export const getPosts = async (req, res) => {
         console.log(postMessage)
         res.status(200).json(postMessage)
     } catch (error) {
-        res.status(404).json({ message: error.message })
+        res.status(404).json(error)
     }
 }
 
@@ -17,6 +18,15 @@ export const createPost = async (req, res) => {
         await newPost.save()
         res.status(201).json(newPost)
     } catch (error) {
-        res.status(409).json({ message: error.message })
+        res.status(409).json(error)
     }
+}
+
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params
+    const post = req.body
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true })
+    res.json(updatedPost)
 }
--- index.js
+++ index.js
@@ -3,18 +3,16 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
-
-dotenv.config()
-
-const app = express()
-
 import postRoutes from './routes/posts.js'
 
-app.use('/posts', postRoutes)
+dotenv.config()
+const app = express()
+app.use(cors())
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors())
+
+app.use('/posts', postRoutes)
 
 const PORT = process.env.PORT || 5000;
 
